b87015e3d1e124e527fe16358a56e92b49db1da2
LankiSer <133566219+LankiSer@users.noreply.github.com>
2023-05-24 22:26:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from main_new_Kr_Drozdova import one_func
 from main_Nikita_ivanchenko import two_func
 from elena_duragina import func_3
-from main_tim import func_4
+
 from main_vladimir import func_5
 
 st.title("Программная инженерия. Практическая работа 10")
